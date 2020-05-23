c298005dda4d25aefc9019947fa2e63e80374767
ERICKVALEN400 <erick.valen@outlook.com>
2020-05-22 20:49:42 -0700

    Updates

---
+++ precache-manifest.380120e6ccd48d8772a5b744084c7c6e.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "75eaadccb893d7c9df7baed4d300b630",
+    "url": "/monsters-rolodex/index.html"
+  },
+  {
+    "revision": "8e480823b528abee7354",
+    "url": "/monsters-rolodex/static/css/main.ad23d5e9.chunk.css"
+  },
+  {
+    "revision": "8fcb5bb5c48e7f651a9e",
+    "url": "/monsters-rolodex/static/js/2.3a034448.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/monsters-rolodex/static/js/2.3a034448.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8e480823b528abee7354",
+    "url": "/monsters-rolodex/static/js/main.ff987d6b.chunk.js"
+  },
+  {
+    "revision": "3087ad26c6aebe6078f8",
+    "url": "/monsters-rolodex/static/js/runtime-main.93b9320d.js"
+  }
+]);
